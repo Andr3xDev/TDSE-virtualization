0fa964144bb55b768a82f3dd0382ffa5be0642ba
Andr3xDev <andr3xdev@gmail.com>
2025-09-11 17:28:26 -0500

    add: initial screens

---
+++ src/main/java/edu/escuelaing/tdse/TdseApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.tdse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/tdse/TdseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.tdse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
